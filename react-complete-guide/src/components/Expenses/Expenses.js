548aa3ee6730d501625c0323bbe1c4a9dd58b1a7
Nakul Sharma <56245793+nakulsharma-tal@users.noreply.github.com>
2021-08-29 11:47:23 +0530

    section-4: completed

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,14 +1,27 @@
+import { useState } from "react";
+
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filterYear, setFilterYear] = useState("2020");
+
+  const filterYearChangeHandler = (newFilterYear) => {
+    setFilterYear(newFilterYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        filterYear={filterYear}
+        onFilterYearChange={filterYearChangeHandler}
+      />
       {props.expenses.map((expense) => {
         return (
           <ExpenseItem
-            id={expense.id}
+            key={expense.id}
             date={expense.date}
             title={expense.title}
             amount={expense.amount}
---
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,21 @@
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const addExpenseHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onAddExpense={addExpenseHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
