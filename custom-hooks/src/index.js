7a92738217d35049111aefec6ba152ddc625bb68
Nakul Sharma <56245793+nakulsharma-tal@users.noreply.github.com>
2021-11-28 16:25:22 +0530

    section-15: starter code

---
+++ custom-hooks/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+reportWebVitals(console.log);
